fe8b6c233e339679e6359e444bacf7c07ecdec4c
Aaron <abrahamaaron70@gmail.com>
2024-06-02 21:21:04 +0000

    modified:   genStr.py

--- genStr.py
+++ genStr.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
-from pyrogram import Client
+from hydrogram import Client
 
 
 api_id = int(input("API ID: "))
