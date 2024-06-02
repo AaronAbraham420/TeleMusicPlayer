758b776c5b02e6cdd98017494e47f462992da0a8
Aaron <abrahamaaron70@gmail.com>
2024-06-02 21:56:12 +0000

    Added a new function

--- core/stream.py
+++ core/stream.py
@@ -23,7 +23,8 @@ from core.song import Song
 from hydrogram import Client
 from yt_dlp import YoutubeDL
 from core.funcs import generate_cover
-from pytgcalls import PyTgCalls, Stream
+from pytgcalls import PyTgCalls
+from pytgcalls.types import MediaStream
 from core.groups import get_group, set_title
 from hydrogram.raw.types import InputPeerChannel
 from hydrogram.raw.functions.phone import CreateGroupCall
