07780a330cd02103f88f591bcb2d3b5d9cdef328
Aaron <abrahamaaron70@gmail.com>
2024-06-02 21:43:40 +0000

    Deleted Stream Type

--- core/stream.py
+++ core/stream.py
@@ -23,7 +23,7 @@ from core.song import Song
 from hydrogram import Client
 from yt_dlp import YoutubeDL
 from core.funcs import generate_cover
-from pytgcalls import PyTgCalls, StreamType
+from pytgcalls import PyTgCalls
 from core.groups import get_group, set_title
 from hydrogram.raw.types import InputPeerChannel
 from hydrogram.raw.functions.phone import CreateGroupCall
